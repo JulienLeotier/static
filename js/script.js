a35405fd537973b3d0c0090934d636407db703c9
JulienLeotier <julien.leotier@gmail.com>
2018-03-25 05:24:37 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ function sendPicture() {
   }
   setTimeout(()=>{
     $.ajax({
-      url: "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description&details=Landmarks&language=en",
+      url: "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description&details=Landmarks&language=fr",
       beforeSend: function (xhrObj) {
         xhrObj.setRequestHeader("Content-type", "application/json");
         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "e161ebfcf86444509fdb1df05381f724");
@@ -60,7 +60,8 @@ function sendPicture() {
       data: '{"url":"http://www.rendreservice.fr:8080/images.png"}',
     })
       .done(function (data) {
-        console.log(data)
+        var hello = document.getElementById("video");
+        hello.innerHTML = "<video  class='responsive-video' style='opacity: 0' controls=''  name='media'><source src='https://code.responsivevoice.org/getvoice.php?t=" + data.description.captions.text +"&amp;tl=fr&amp;sv=&amp;vn=&amp;pitch=0.5&amp;rate=0.5&amp;vol=1' type='audio/mpeg'></source></video"
       })
       .fail(function () {
         console.log('toto')
