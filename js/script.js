8628f6be5b21ee7d8ebe3052f595a073264dcd4f
JulienLeotier <julien.leotier@gmail.com>
2018-03-25 08:01:59 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let width = 500,
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas')
 
-navigator.mediaDevices.getUserMedia({ audio: true, video: { facingMode: { exact: "environment" } } })
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   .then(function (stream) {
     video.srcObject = stream
     video.play()
