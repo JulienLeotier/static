6d7a6baeebe48dd10df27da5cf408c0973a73828
JulienLeotier <julien.leotier@gmail.com>
2018-03-25 03:50:01 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ video.addEventListener('canplay', function (e) {
 }, false)
 
 function takePicture() {
-  // Create canvas
   const context = canvas.getContext('2d')
   if (width && height) {
 
@@ -35,11 +34,6 @@ function takePicture() {
     canvas.height = height
 
     context.drawImage(video, 0, 0, width, height)
-
-    // canvas.toBlob(function (blob) {
-
-    //   saveAs(blob, "image.png")
-    // });
     const imgUrl = canvas.toDataURL('image/jpeg')
 
     return imgUrl
@@ -49,41 +43,29 @@ function takePicture() {
 function sendPicture() {
   let picture = takePicture()
   let xhr = new XMLHttpRequest()
-  xhr.open('POST', 'http://localhost/serverImages', true)
-  xhr.onloadend= () => {
-    console.log('ok')
+  xhr.open('POST', 'https://www.rendreservice.fr/images/', true)
+  xhrObj.setRequestHeader("Content-type: application/json");
+  let data = {
+    'img': picture
   }
+  xhr.send(JSON.stringify(data))
+  xhr.onloadend = (response) => {
+    console.log(response)
+    $.ajax({
+      url: "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags&subscription-key=e161ebfcf86444509fdb1df05381f724",
+      beforeSend: function (xhrObj) {
+        xhrObj.setRequestHeader("Content-type: application/json");
+      },
+      type: "POST",
+      data: '{"url": "https://www.rendreservice.fr:8080/images.jpeg"}',
+    })
+      .done(function (data) {
+        console.log(data)
+      })
+      .fail(function () {
 
-  xhr.send(form)
-
-  // let img = takePicture()
-
-  // setTimeout(function () {
-  //   let params = {
-  //     // Request parameters
-  //     "visualFeatures": "Categories,Description,Color",
-  //     "details": "",
-  //     "language": "en",
-  //   };
-  //   $.ajax({
-  //     url: "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags&subscription-key=e161ebfcf86444509fdb1df05381f724",
-  //     beforeSend: function (xhrObj) {
-  //       // Request headers
-  //       //xhrObj.setRequestHeader("Content-Type","application/json");
-  //       xhrObj.setRequestHeader("Content-type: application/json");
-  //       // xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "e161ebfcf86444509fdb1df05381f724");
-  //     },
-  //     type: "POST",
-  //     // Request body
-  //     data: '{"url": ' + '"' + document.getElementById('url').url + '"}',
-  //   })
-  //     .done(function (data) {
-  //       console.log(data)
-  //     })
-  //     .fail(function () {
-
-  //     });
-  // }, 1000)
+      });
+  }
 }
 
 
